29a70502c4c4da20da58ea7d39bafb65b0cbad78
Mayur Mahrotri <mayurmahrotri@gmail.com>
2016-06-04 18:41:37 +0530

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: mayur
+ *
+ * Created on 4 June, 2016, 6:31 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
